ffe7b3b522373ef09fe942c2edc9f96c607a887a
Lucas Lannes <lucaslannesr@gmail.com>
2025-10-25 11:57:23 -0300

    Add prompt parameter to generate metadata

--- scripts/cli/add_metadata.py
+++ scripts/cli/add_metadata.py
@@ -5,13 +5,14 @@ import re
 
 def main(argv):
     if len(argv) < 4:
-        print("Usage: python scripts/add_version.py <locale> <model> <source> <attribution?>")
+        print("Usage: python scripts/add_version.py <source> <locale> <model> <prompt?> <attribution?>")
         sys.exit(1)
 
-    locale = argv[1]                    
-    model = argv[2]
-    source = argv[3].split(",")
-    attribution = argv[4] if len(argv) > 4 else ""
+    source = argv[1].split(",")
+    locale = argv[2]                    
+    model = argv[3]
+    prompt = argv[4] if len(argv) > 4 else ""
+    attribution = argv[5] if len(argv) > 5 else ""
 
     version = (" ".join([*source, model])).upper()
     version = re.sub(r'[^a-zA-Z0-9]', ' ', version).strip()
@@ -20,6 +21,7 @@ def main(argv):
     print(f"attribution: {attribution}")
     print(f"locale: {locale}")
     print(f"model: {model}")
+    print(f"prompt: {prompt}")
     print(f"source: {source}")
     print(f"version: {version}")
 
@@ -31,6 +33,7 @@ def main(argv):
             "attribution": attribution,
             "locale": locale,
             "model": model,
+            "prompt": prompt,
             "source": source,
             "version": version,
         }
